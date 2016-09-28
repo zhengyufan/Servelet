d5a7edda2fa35984bcd51ca5ecddecd3a4fb8a2f
Yufan Zheng <379311562@qq.com>
2016-09-28 15:19:05 +0200

    Add an index.jsp and a GetName serverlet to get user input first name and last name

---
+++ src/com/servelet/example/GetNames.java
@@ -0,0 +1,21 @@
+package com.servelet.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/get-names")
+public class GetNames extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		response.getWriter().println("Hello " + firstname + " " + lastname + "!");
+	}
+
+}
