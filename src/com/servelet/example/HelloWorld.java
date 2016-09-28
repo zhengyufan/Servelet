16f3e4be0cd3c5ae1094efd46c9cbe7d32d145c0
Yufan Zheng <379311562@qq.com>
2016-09-28 14:57:45 +0200

    A hello World J2EE Servelet Example

---
+++ src/com/servelet/example/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.servelet.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello-world")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hello World!");
+	}
+
+}
